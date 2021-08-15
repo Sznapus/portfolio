83fce55a23c32fc5f9e1baf039fa513941c8af8a
Sznaps <robsznapka@gmail.com>
2021-08-15 06:24:00 +0200

    Start styling and build structure

---
+++ script/main.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector('.hamburger'); 
+const navigation = document.querySelector('nav');
+const li = document.querySelectorAll('nav li');
+
+menu.addEventListener('click', () => { 
+  menu.classList.toggle('hamburger--active'); 
+  navigation.classList.toggle('active');
+  li.forEach(item => {
+    item.addEventListener('click', () =>{
+    menu.classList.remove('hamburger--active');
+    navigation.classList.remove('active');
+    })
+  }); 
+});
